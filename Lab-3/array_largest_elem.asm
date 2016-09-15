afbe766d94aaa0619aadc58ea48053d1536dbde0
Mohammed Sahil <sahil-mohd@users.noreply.github.com>
2016-09-15 01:13:01 -0700

    Create array_largest_elem.asm

---
+++ Lab-3/array_largest_elem.asm
@@ -0,0 +1,41 @@
+.data
+	list: .word 11, 27, 13, 4, 25, 26, 57, 60, 59, 10
+	size: .word 10
+
+	spc: .asciiz "\t"
+	msg_res: .asciiz "The largest element is: "
+
+.text 
+.globl main
+main:	lw $t3, size	# Limit
+		la $t1, list	# Array
+		li $t2, 0		# Index
+		li $t4, 0		# max
+
+		l1: beq $t2, $t3, l2
+			
+			lw $a0, ($t1)
+			li $v0, 1		# Print element
+			syscall
+			li $v0,4
+			la $a0,spc		# Print space
+			syscall
+
+			lw $t5, ($t1)
+			blt $t5, $t4, gr
+			move $t4,$t5
+			gr:
+			addi $t2, $t2, 1
+			addi $t1, $t1, 4
+
+			j l1
+
+		l2:	
+			li $v0, 4
+			la $a0, msg_res
+			syscall
+			li $v0, 1
+			move $a0, $t4
+			syscall
+			li $v0,10
+			syscall
