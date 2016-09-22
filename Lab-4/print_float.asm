425abead2dc4beba509fcf1b550b8a531c3e37fa
Mohammed Sahil <sahil-mohd@users.noreply.github.com>
2016-09-22 00:27:07 -0700

    Create print_float.asm

---
+++ Lab-4/print_float.asm
@@ -0,0 +1,21 @@
+.data
+	val: .float -6.12
+	lfeed: .asciiz "\n"
+
+.text 
+.globl main
+main:
+	l.s $f12, val
+	li $v0, 2
+	syscall
+
+	li $v0, 4
+	la $a0, lfeed
+	syscall
+
+	lw $a0, val
+	li $v0, 1
+	syscall
+
+	li $v0, 10
+	syscall
