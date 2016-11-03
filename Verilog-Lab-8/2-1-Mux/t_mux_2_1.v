f953d6cdb95e3df885502d5680616d7754889b3d
Mohammed Sahil <sahil-mohd@users.noreply.github.com>
2016-11-03 00:58:57 -0700

    Update t_mux_2_1.v

--- Verilog-Lab-8/2-1-Mux/t_mux_2_1.v
+++ Verilog-Lab-8/2-1-Mux/t_mux_2_1.v
@@ -4,24 +4,24 @@ module t_mux_2_1;
 
   mux_2_1 S1(out, a, b, S );
   initial begin
-    a = 0; b = 0; S = 0;
+    S = 0; a = 0; b = 0;
     #50
-    a = 0; b = 0; S = 1;
+    S = 0; a = 0; b = 1;
     #50
-    a = 0; b = 1; S = 0;
+    S = 0; a = 1; b = 0;
     #50
-    a = 0; b = 1; S = 1;
+    S = 0; a = 1; b = 1;
     #50
-    a = 1; b = 0; S = 0;
+    S = 1; a = 0; b = 0;
     #50
-    a = 1; b = 0; S = 1;
+    S = 1; a = 0; b = 1;
     #50
-    a = 1; b = 1; S = 0;
+    S = 1; a = 1; b = 0;
     #50
-    a = 1; b = 1; S = 1;
+    S = 1; a = 1; b = 1;
     #50
-    a = 0; b = 0; S = 0;
+    S = 0; a = 0; b = 0;
   end
   initial
-  $monitor("| A = %b | B = %b | S = %b | out = %b | time = %0d |", a, b, S, out, $time);
+  $monitor("| S = %b | A = %b | B = %b | out = %b | time = %0d |", S, a, b, out, $time);
 endmodule
